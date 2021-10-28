28f7dc76fbe43c7bba9223e4c230d1dd1fbbc3e9
Jan Schröder <mail@janschroeder.eu>
2021-10-28 21:08:13 +0200

    added p5

--- components/Page.jsx
+++ components/Page.jsx
@@ -1,25 +1,28 @@
 import Link from "next/link";
 
 function Page({ children }) {
-	return (
-		<>
-			<div className="m-8">
-				<div>
-					<Link href="/">
-						<span className="mr-8 cursor-pointer">Home</span>
-					</Link>
-					<Link href="/garden">
-						<span className="mr-8 cursor-pointer">Garden</span>
-					</Link>
-				</div>
-			</div>
-			{/* todo: make site searchable */}
-			{/* <div className="m-8">
+  return (
+    <>
+      <div className="m-8">
+        <div>
+          <Link href="/">
+            <span className="mr-8 cursor-pointer">Home</span>
+          </Link>
+          <Link href="/garden">
+            <span className="mr-8 cursor-pointer">Garden</span>
+          </Link>
+          <Link href="/p5">
+            <span className="mr-8 cursor-pointer">P5</span>
+          </Link>
+        </div>
+      </div>
+      {/* todo: make site searchable */}
+      {/* <div className="m-8">
 				<p>Search</p>
 			</div> */}
-			{children}
-		</>
-	);
+      {children}
+    </>
+  );
 }
 
 export default Page;
